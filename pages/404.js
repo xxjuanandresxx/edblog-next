b35458f76bb2711ae5e195ff8051296c7e309c30
Juan Andres Castillo Soria <j.andrescastillo.711@gmail.com>
2021-02-09 17:26:20 -0400

    Add custom 404 error

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div className="ed-grid s-py-4">
+      <h1>OPS! Error 404</h1>
+      <span>La página solicitada no existe</span>
+    </div>
+  )
+}
