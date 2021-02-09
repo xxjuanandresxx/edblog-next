085eb5dafc74439712e8ac0794f10035fa1a6487
Juan Andres Castillo Soria <j.andrescastillo.711@gmail.com>
2021-02-09 17:26:06 -0400

    Added SideMenu for small view

--- components/navigation/MainMenu.jsx
+++ components/navigation/MainMenu.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { createRef } from 'react'
 import Link from 'next/link'
+
+const menu = createRef()
+
+function toggleMenu() {
+  menu.current.classList.toggle("show")
+}
 
 export default function MainMenu() {
   return (
     <div className="s-grid-1 lg-cols-3 s-cross-center s-main-end header-links">
-      <nav className="main-menu">
+      <nav className="show main-menu" ref={menu}>
         <ul>
           <li><Link href="/">Home</Link></li>
           <li><Link href="/about">About</Link></li>
           <li><Link href="/contact">Contact</Link></li>
         </ul>
       </nav>
+      <div 
+        className="main-menu-toggle to-l"
+        onClick={() => toggleMenu()}
+      ></div>
     </div>
   )
 }
