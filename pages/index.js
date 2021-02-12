a60ca12c4098d4766fbca8f0822847ad2e13b057
Juan Andres Castillo Soria <j.andrescastillo.711@gmail.com>
2021-02-12 19:48:04 -0400

    Use NextSEO component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
-import Head from 'next/head'
 import fetch from 'node-fetch'
 import Post from '../components/blog/Post'
+import { NextSeo } from 'next-seo'
 
 export default function Home({ posts }) {
   return (
     <div>
-      <Head>
+      {/* <Head>
         <title>Home | {process.env.SITE_NAME}</title>
-      </Head>
+      </Head> */}
+      <NextSeo
+        title = "Bienvenido a mi Blog"
+        description = "EDBlog es una creación del curso de NextJS de EDTeam"
+        openGraph = {{
+          url : "https://ed.team",
+          title : "Bienvenido a EDBlog",
+          description : "EDBlog es una creación del curso de NextJS de EDTeam",
+          images : [
+            {
+              url : "https://ed.team/images/algo.jpg",
+              width : 1280,
+              height : 720,
+              alt: "Foto de EDTeam"
+            }
+          ],
+          site_name : "EDBlog"
+        }}
+        twitter = {{
+          handle : "@edteamlat",
+          site : "@edteamlat",
+          cardType : "summary_large_image" 
+        }}
+      />
       <main>
         <h1>EDBlog</h1>
         <div className="ed-grid m-grid-3 row-gap">
